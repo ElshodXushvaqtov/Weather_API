a4734869ddbf38909af2cef15e5564c0fbd631b4
User_4 <elshodxushvaqtov07@gmail.com>
2023-10-09 11:24:14 +0500

    Finished Api-Weather

--- app/src/main/java/com/example/weatherapp/Forecast.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weatherapp
-
-data class Forecast(
-    val humidity: String, val wind_mps: Double, val pressure_mb:Double, var cloud:Int, val location: String
-)
--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -14,31 +14,62 @@ import java.net.URL
 import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var address: String
+    private lateinit var city: String
     private var humidity by Delegates.notNull<Int>()
-    private var wind by Delegates.notNull<Double>()
+    private lateinit var wind: String
+    private var temp by Delegates.notNull<Double>()
+    private lateinit var last_updated: String
+    private lateinit var local_time: String
+    private lateinit var status: String
+    private var pressure_mb by Delegates.notNull<Double>()
+    private lateinit var sunrise: String
+    private lateinit var sunset :String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        address = binding.address.text.toString()
-        val url =
-            "http://api.weatherapi.com/v1/current.json?Key=542c36dc84064b1fa22132223230810&q=Tashkent"
+
         val requestQueue = Volley.newRequestQueue(this)
-        val request = JsonObjectRequest(url, object : Response.Listener<JSONObject> {
-            override fun onResponse(response: JSONObject?) {
-                val main = response?.getJSONObject("main")
-                address = main?.getString("name").toString()
-                binding.address.text = address
+        binding.searchBtn.setOnClickListener {
+            val url =
+                "http://api.weatherapi.com/v1/forecast.json?Key=542c36dc84064b1fa22132223230810&q=${binding.address.text}"
+            val request = JsonObjectRequest(url, object : Response.Listener<JSONObject> {
+                override fun onResponse(response: JSONObject?) {
+                    val location = response!!.getJSONObject("location")
+                    val forecast = response.getJSONObject("current")
+                    val weather_condition =
+                        response.getJSONObject("current").getJSONObject("condition")
+                    city = location.getString("name")
+//                val sun_info = response.getJSONObject("forecast").getJSONObject("forecastday").getJSONObject("astro")
+                    humidity = forecast.getInt("humidity")
+                    temp = forecast.getDouble("temp_c")
+                    local_time = location.getString("localtime")
+                    last_updated = forecast.getString("last_updated")
 
-            }
-        }, object : Response.ErrorListener {
-            override fun onErrorResponse(error: VolleyError?) {
-                Log.d("BBB", "onResponse: $error")
-            }
-        })
-        requestQueue.add(request)
+                    status = weather_condition.getString("text")
+                    wind = forecast.getString("wind_mph")
+                    pressure_mb = forecast.getDouble("pressure_mb")
+//                sunrise = sun_info.getString("sunrise")
+//                sunset = sun_info.getString("sunset")
+                    binding.lastUpdated.text = last_updated
+                    binding.localDate.text = local_time
+                    binding.temp.text = temp.toString()
+                    binding.status.text = status
+                    binding.humidity.text = humidity.toString()
+                    binding.wind.text = wind
+//                binding.sunrise.text = sunrise
+                    binding.pressure.text = pressure_mb.toString()
+//                binding.sunset.text = sunset
+                }
+            }, object : Response.ErrorListener {
+                override fun onErrorResponse(error: VolleyError?) {
+                    Log.d("BBB", "onResponse: $error")
+                }
+            })
 
+            requestQueue.add(request)
+
+        }
     }
-
 }
